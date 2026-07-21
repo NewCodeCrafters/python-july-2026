5f31770352ad093ffa4c0fa3192d281d821964f9
mayowadamilare25-web <mayowadamilare25@gmail.com>
2026-07-21 12:18:34 +0100

    done

--- Emmanuel.py
+++ Emmanuel.py
@@ -1,2 +1,37 @@
-print("Hello My Name is Emmanuel, i am a backend developer")
-print("I have experience in Python, Django, and Flask frameworks.")
+# string
+name = 'bello'
+first_name = 'Olamide'
+lastName = '13'
+DEPARTMENT = 'Computer'
+
+                 # NUMERIC DATA-TYPES
+ #intergerrs and floats
+
+ #intergers
+age = 25
+count = -10
+
+# floats
+price = 19.99
+temperature = -5.5
+num1 = 9.0
+print(type(age))
+print(type(lastName))
+print(type(num1))
+
+#boolean
+is_active = True
+has_permission = False
+
+print(type(is_active))
+print(type(has_permission))
+                  
+
+
+
+#Variables Concatenation / Chaining / Linking
+first_name = "Bello"
+last_name = "AbdulHakeem"
+sentence = "My name is " + first_name + " " + last_name
+print(sentence)
+sentence = f"my name is {first_name} {last_name}"
