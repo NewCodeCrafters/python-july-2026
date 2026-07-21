789ea4ed9deb5891a6b8e4542c2a79bb49c33b02
destiny-jpg234 <ebipamowiedidekemiemo@gmail.com>
2026-07-21 23:35:38 +1200

    message added

--- destiny.py
+++ destiny.py
@@ -1,2 +1,38 @@
-print("destiny is a cybersecurity analysis")
-print("Millions of people use Python. This means you can easily find free code, tutorials, and answers to your questions online.")
+#string
+name = "bello"
+firt_name ='olamide' 
+lastName = "13"
+DEPARTMENT ='computer'
+
+# Numeric Datatype
+
+# Integers and floats
+
+# Integers
+age = 25
+count = -10
+
+#Float
+price = 19.9
+temperature = -5.5
+num1 = 9.0
+
+print(type(age))
+print(type(lastName))
+print(type(num1))
+
+#boolan
+is_active =True
+has_permission = False
+
+print(type(is_active))
+print(type(has_permission))
+
+#variables concatenation /chaning /linking
+
+firt_name = "bello"
+lastName = "AbdulKaeem"
+sentence = "my name is" +firt_name +" "last_name
+print(sentence)
+sentence =f"my name is{firt_name} {last_name}"
+print(sentence)
