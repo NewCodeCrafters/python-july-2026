abf762d5da6a33cc0bb1989a0e467258ab0fe9b1
muhammad <muhammadsaninuhu57@gmail,com>
2026-07-26 12:00:35 +0100

    finished

---
+++ muhammad/muhammads assignment.py
@@ -0,0 +1,18 @@
+first_name="nuhu"
+last_name="muhammad"
+sentence=f"my full name is{first_name}" "{last_name}"
+print ("sentence")
+
+
+favoritecity ="badagry"
+print("favoritecity")
+
+favoritecity=favoritecity.upper()
+print("favoritecity")
+
+favoritecity=favoritecity.lower()
+print("favoritecity")
+
+favoritecity=favoritecity.title()
+print("favoritecity")
+
