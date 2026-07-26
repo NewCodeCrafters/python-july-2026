5bf4e0edc34f4bc586ed5d9d7152b149a65b25ac
malikolamilekan3916-star <malikolamilekan3916@gmail.com>
2026-07-27 00:29:39 +1300

    assesment

--- malik.py
+++ malik.py
@@ -1,9 +1,29 @@
-print("my name is malik, i am  cyber security analyst")
-print()# Variables Concatenation / Chaining / Linking
+# String
+name = "Morakinyo" 
+first_name = 'Malik'
+last_name = "Olamilekan"
+DEPARTMENT= 'Computer'
 
-first_name = "Morakinyo"
-last_name = "AbdulMalik"
-sentence = "My name is " + first_name + " " + last_name
-print(sentence)
-sentence = f"My name is {first_name} {last_name}"
-print(sentence)
+# Numeric DataType 
+
+# Integers and Floats
+
+# Integers
+age = 18
+count = -10
+
+# Floats
+price = 19.99
+temperature = -5.5
+num1 = 9.0
+
+print(type(age))
+print(type(last_name))
+print(type(num1))
+
+# Boolean
+is_active = True
+has_permission = False
+
+print(type(is_active))
+print(type(has_permission))
