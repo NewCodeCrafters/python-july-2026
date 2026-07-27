a23e1a6da2e24355310a9a8942eec34dc4433542
malikolamilekan3916-star <malikolamilekan3916@gmail.com>
2026-07-27 22:39:07 +1300

    assesment

---
+++ malik/assesment.py
@@ -0,0 +1,10 @@
+# variable
+# exercise 1
+full_name = "Morakinyo"
+print(len(full_name))
+# exercise 2
+print(full_name[0:-6])
+# exercise 3
+print (full_name[0:9])
+# exercise 4
+print(full_name[::3])
