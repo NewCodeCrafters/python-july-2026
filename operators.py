22eddf98676ce230de3346837759895bc960faed
servercraft-stack <davidonyekachi29@gmail.com>
2026-07-27 12:12:53 +0100

    pushed

---
+++ operators.py
@@ -0,0 +1,26 @@
+# addition = "+"
+# subtraction = "-"
+# multiplication = "*"
+# division = "/"
+
+
+num1 = int(input("enter a number: "))
+operator = input("choose your operator: ")
+num2 = int(input('enter a number: '))
+
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("invalid input")
+
+
+
+
+
