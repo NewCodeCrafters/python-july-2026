ff1e0b2d1763fd1b5c62e04b94a92facfc204235
akapoayomide07-bit <akapoayomide07@gmail.com>
2026-07-30 16:08:14 +0100

    assignment.py

---
+++ ayomide/assignment.py
@@ -0,0 +1,41 @@
+# username = "Admin"
+# Password = "12345"
+
+# username = input("enter the username: ")
+# Password = int(input("enter the Password: "))
+
+# if username.lower() == "admin" and Password == 12345:
+#     print("Login successful")
+# else:
+#     print("Login failed")
+
+# import random
+# Game = input("Guessing Game, You can never win, Press enter: ")
+# guess = int(input("Enter any number between 1 and 10: "))
+# secret_number = random.randint(1, 10)
+# if guess == secret_number:
+#     print (f"Correct you guessed the secret_number and won N100 million withdraw from your account, The secret number is {secret_number}")
+# else: 
+#     print(f"Wrong, You failed,Tricked by Ayomide, Try again,The secret_number is {secret_number}")
+
+# import random 
+# Animal = ["Cat", "Dog", "Rabbit", "Lion", "Elephant"]
+# guess = input("Enter any animal that comes to your mind: ")
+# Animal = random.choice(Animal)
+# if guess.lower() == Animal.lower():
+#     print(f"You guessed correctly, You won ")
+# else:
+#     print(f"You failed, The correct Animal is {Animal}") 
+
+
+# amount = float(input("Enter the total shopping amount: "))
+
+
+# if amount >= 1000:
+#     print(f"Congratulations! You get a 20% discount.")
+# elif amount >= 500:
+#     print(f"Great! You get a 10% discount.")
+# elif amount >= 200:
+#     print(f"Good! You get a 5% discount.")
+# else:
+#     print(f"Sorry, no discount is available.")
