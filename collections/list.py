d05dee74e6e343b0b11f73b7bd595d9a312ee11d
spiderxploit <cyberdon43@gmail.com>
2026-08-03 12:09:49 +0100

    collections intro

---
+++ collections/list.py
@@ -0,0 +1,30 @@
+# list
+names = ["Uthman", "Emmanuel", "Muhammed", "Destiny", "Ayomide", "Maleek", 90, True, 192.12]
+nums = [13, 12, 92, 13, "Love", "Demon"]
+words = [names, nums]
+
+# print(words[0][5][-1])
+# print(words[1][5][2])
+# print(words[-2][-9][-4:-2])
+# print(words[-2][-6][-3:-1])
+
+# arbitrary, mutable, indexed and sliced, orderable
+
+names.remove(90)
+names.pop()
+names.pop()
+names.append("AbdulHakeem")
+names.extend(["Joseph", "Anne"])
+names.insert(0, "Gideon")
+print(names)
+
+names = sorted(names)
+
+print(names)
+names = sorted(names, reverse=True)
+
+print(names)
+
+nums = [-1, 19, 25, True, 94, 0, False]
+
+print(sorted(nums))
