615c79b6433354186903fb3f46180b30382cdedc
destiny-jpg234 <ebipamowiedidekemiemo@gmail.com>
2026-08-11 00:02:48 +1200

    updated

---
+++ destiny/.good.py
@@ -0,0 +1,11 @@
+numbers = 0
+
+while numbers <=20:
+    numbers += 2
+    print(numbers)
+
+
+
+
+
+
---
+++ destiny/me.py
@@ -0,0 +1,11 @@
+numbers = [12, 45, 7, 89, 23, 56]
+
+for numbers in range  (89):
+    if numbers >=89:
+     print(numbers)
+
+
+
+
+
+
