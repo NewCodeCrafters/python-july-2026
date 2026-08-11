8fea705174e3fbd4e77367e3a2c6f9cd93d4a7b5
mayowadamilare25-web <mayowadamilare25@gmail.com>
2026-08-11 13:10:45 +0100

    pushed

--- Emmanuel/assignment3.py
+++ Emmanuel/assignment3.py
@@ -1,11 +1,11 @@
 names = ["Uthman", "Emmanuel", "Muhammed", "Destiny", "Ayomide", "Maleek",
          90, True, 192.12] 
 
-names.insert(0,"Gideon")
+# names.insert(0,"Gideon")
 
-print(names[3][3:6])
+print(names[3][3:-1])
 
-print(names)
+# print(names) 
 
 
    
--- collections/tuples.py
+++ collections/tuples.py
@@ -2,3 +2,4 @@ names = ("Uthman", "Emmanuel", "Muhammed", "Destiny", "Ayomide", "Maleek")
 
 print(names.count("Muhammed"))
 
+ 
