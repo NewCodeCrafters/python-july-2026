6bafe4c3b5e6a7a5fb12113064e959a16c3aed42
malikolamilekan3916-star <malikolamilekan3916@gmail.com>
2026-08-13 00:23:46 +1300

    done

---
+++ malik/class2.py
@@ -0,0 +1,11 @@
+# Write a function that receives two numbers and prints their sum.
+def print_sum(num1,num2):
+    total = num1 + num2
+    print(total)
+
+# Write a function that receives one number and prints **Even** if the number is even, otherwise print **Odd**.
+def check_even_odd(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
