691328b28f7cbd419825cdf07a49a435026c83df
destiny-jpg234 <ebipamowiedidekemiemo@gmail.com>
2026-08-13 10:09:17 +0100

    message updaded

--- destiny/.good.py
+++ destiny/.good.py
@@ -1,4 +1,283 @@
-number = 2
-while number <= 20:
-    print(number)
-    number += 2
+# number = 2
+# while number <= 20:
+#     print(number)
+#     number += 2("enter number here:"))
+
+
+
+
+# def score_student(score):
+#     if score >=50:
+#         print("pass")
+#     elif score <=49:
+#         print("fail")  
+#     else:
+#         print("invalid")
+
+# score = int(input("enter:"))
+# score_student(score)
+
+
+# def cube(number):
+#     print(number * number * number)
+
+# cube(4)
+
+# Write a function that receives the number of hours worked and hourly pay and returns the total salary.
+# Write a function that receives a score and returns the corresponding letter grade using A, B, C, D, and F.
+# Write a function that receives two strings and returns them joined with a space between them.
+# Write a function that receives a number and returns the multiplication table from 1 to 12 as a list.
+# Write a function that receives a list of numbers and returns how many numbers are even
+
+
+
+# def salary_pays(hours,pay):
+#     if hours in pay == 20:
+#      return hours*pay
+
+# print(salary_pays(2,20))
+
+
+# def score(grade):
+
+#     if grade>=70:
+
+#         print(f"pass grade A")
+#     elif grade >=68:
+#         print(f"pass grade B")
+#     elif grade >=60:
+#         print(f"grade C")
+#     elif grade >=50:
+#         print(f"grade D")
+#     else:
+#         print(f"your falied grade F")            
+
+
+# grade=int(input("your grade here:"))
+
+# score(grade)
+
+
+
+# def word_combine(word1,word2):
+
+#     return word1 +""+ word2
+# print(word_combine("help","good"))
+
+
+
+def count_even(numbers):
+    count =0
+
+    for number in numbers:
+
+        if  number%2 ==0:
+            count +=1
+    return count
+print(count_even([1, 2, 3, 4, 5, 6]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
