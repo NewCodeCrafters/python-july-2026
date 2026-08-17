653f9444cf236f37cb3d09fec597d01b77dd24f2
mayowadamilare25-web <mayowadamilare25@gmail.com>
2026-08-17 10:28:08 +0100

    pushed

---
+++ Emmanuel/assesment7.py
@@ -0,0 +1,44 @@
+# def item_price (item, price,discounted_price, discount_percentage):
+#     item = input("Enter the item name: ")
+#     price = float(input("Enter the item price: "))
+#     discount_percentage = price * 0.10
+#     discounted_price = price - discount_percentage
+
+#     print(f"The item name is {item}")
+#     print(f"The item price is {price}")
+#     print(f"The discount percentage is {discount_percentage}")
+#     print(f"The discounted price is {discounted_price}")
+
+# item_price("item", "price", "discounted_price", "discount_percentage")
+
+
+
+# def my_age (age):
+#     age = int(input("Enter your age: "))
+#     if age < 18:
+#         print("You are a minor.")
+#     elif 18 <= age < 65:
+#         print("You are an adult.")
+#     else:
+#         print("You are a senior citizen.")
+
+# my_age("age")
+
+
+
+# Write a function that receives two numbers and returns both the quotient and remainder when the first is divided by the second.
+
+def division(first_number, second_number, quotient, remainder):
+    first_number = int(input("Enter your first number: "))
+    second_number = int(input("Enter your second number: "))
+
+    quotient = first_number // second_number
+    remainder = first_number % second_number
+
+    print(quotient)
+    print(remainder)
+
+division("first_number", "second-number", "quotient", "remainder")
+
+
+
