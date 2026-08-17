57d15b9a09b9360019726c0a446e78646aeeac40
dev-ore911 <oresofoluwe@gmail.com>
2026-08-17 12:54:57 +0100

    pushed

---
+++ oreoluwa/assessment3.py
@@ -0,0 +1,28 @@
+'''import math
+
+def circumference(radius):
+    return 2 * math.pi * radius
+
+radius = float(input("Enter the radius of the circle: "))
+
+print(circumference(radius))'''
+
+#Write a function that receives a number and returns the factorial of that number.
+'''def print_number(n):
+
+
+
+
+n = int(input("Enter a number: "))'''
+
+
+#Write a function that receives a list of numbers and returns the average of the numbers.
+
+#Write a function that receives a password string and returns whether its length is at least 8 characters.
+def check_password(password):
+    return len(password) >= 8
+
+pw = input("Enter a password: ")
+print(check_password(pw))
+
+#Write a function that receives a year and returns whether it is a leap year.
