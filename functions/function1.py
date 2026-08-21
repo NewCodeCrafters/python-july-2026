281cf3336b63f46c6e0849aac08ab87fc8750b36
spiderxploit <cyberdon43@gmail.com>
2026-08-21 12:43:17 +0100

    functions

---
+++ functions/function1.py
@@ -0,0 +1,9 @@
+# def greet_user(name):
+#     print(f"Hello, {name}!")
+
+# greet_user("Joe")
+
+def add_numbers(a, b, c, d):
+    return a + b - c / d
+result = add_numbers(5, 3, 2, 4)
+print(result)
