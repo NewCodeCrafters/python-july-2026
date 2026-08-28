2d82502a02fb51b637158228dd676814ac89cc35
destiny-jpg234 <ebipamowiedidekemiemo@gmail.com>
2026-08-28 18:04:30 +0100

    update

--- destiny/me.py
+++ destiny/me.py
@@ -1,4 +1,180 @@
-number = 2
-while number <= 20:
-    print(number)
-    number += 2
+# # number = 2
+# # while number <= 20:
+# #     print(number)
+# #     nur += 2
+# def bus(cap, on, wait):
+#     space = cap - on
+
+#     if wait <= space:
+#         return 0
+#     else:
+#         return wait - space
+
+
+# # print(bus(20, 15, 3Use indexing to print John's name.
+# Use negative indexing to print Sarah's information.
+# Use slicing to create a list containing only the first 2 students.
+# Use a loop to print every student's name.
+# Create a function average_score() that calculates each student's average score.
+# Use a loop and the function to print each student's name and average.
+# Use the dictionary to print Mary's Python score.
+# Find and print the student with the highest Python score.
+
+# students = [
+#     ("John", 18, {"math": 75, "english": 80, "python": 90}),
+#     ("Mary", 20, {"math": 85, "english": 70, "python": 95}),
+#     ("David", 19, {"math": 60, "english": 90, "python": 75}),
+#     ("Sarah", 21, {"math": 95, "english": 85, "python": 88})
+# ]
+
+# # print (students[0][0])
+# # print(students[-1])
+# # print(students[:2])
+
+# # for student in students:
+# #     print(students[0])
+
+# def  average_score(scores):
+#     total=0
+#     ce = student[0]
+#     scores = student[2]
+
+#     average = average_score(scores)ount=0
+#     for score in scores:
+#         total=+1
+#         count=+1
+#     return total / count
+
+
+# for student in students:
+#     nam
+
+
+
+# numbers = [10, 20, 30, 40, 50]
+
+# for n in numbers :
+#     print(numbers)
+
+
+
+# colors = ("red", "blue", "green", "yellow")
+
+# print(colors[1])
+# print(colors[3])
+
+# def check_number(number):
+#     if number% 2==0:
+#         print("even")
+#     else:
+#         print("odd")
+# number=int(input("enter number here:"))
+# check_number(number)
+
+# def find_sum(numbers):
+#     total=0
+
+#     for number in numbers:
+#         total+= number
+
+#     return total
+
+# print(find_sum([2, 4, 6]))
+ 
+# numbers = [5, 12, 8, 20, 3, 15]
+# print(max(numbers))
+
+# numbers = [5, 12, 8, 20, 3, 15]
+# for number in numbers:
+#     if number>=10:
+
+#       print(number)
+
+
+
+# students = ("John", "Mary", "David", "Sarah")
+# print(students[0])
+# print(students[1])
+# print(students[2])
+# print(students[3])
+
+
+# def count_even(numbers):
+#     count=0
+# for number in numbers:
+#     if number %2 ==0:
+#         count=+1      
+#     return count
+
+
+
+# def count_even(numbers):
+#     count = 0
+
+#     for number in numbers:
+#         if number % 2 == 0:
+#             count += 1
+
+#     return count
+
+
+# print(count_even([2, 5, 8, 11, 14]))
+
+
+
+
+# def count_even(numbers):
+#     count = 0
+
+#     for number in numbers:
+#         if number % 2 == 0:
+#             continue
+#         else:
+#             count +=1
+#     return count
+
+
+# print(count_even([2, 5, 8, 11, 14]))
+
+
+def check_type(date):
+
+    date=(10.5 ,"hello",[1, 2, 3])
+10.5
+"Hello"
+print(type( "date:"))
+print(type(10.5))   
+print(type("hello"))
+print(type([1, 2, 3]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
